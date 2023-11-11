c2caabbc6ed6044196474d85cbb2786bdc95de75
jakob753951 <jlm1999@live.dk>
2023-11-11 09:21:44 +0100

    Added two new endpoints and changed som POST to PUT

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ pub struct User {
 	id: Option<ObjectId>,
 	name: String,
 	phone: String,
-	group_ids: Vec<ObjectId>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -56,7 +55,7 @@ impl Api {
 		Json(users)
 	}
 
-	/// Get user by id
+	/// Get user by ID
 	#[oai(path = "/users/:id", method = "get")]
 	async fn get_user(&self, database: Data<&Database>, user_id: Path<String>) -> Json<User> {
 		let collection = database.collection::<User>("users");
@@ -72,6 +71,26 @@ impl Api {
 		let result = collection.insert_one(user.0, None).await.unwrap();
 		let result = collection.find_one(doc!{"_id": result.inserted_id}, None).await.unwrap().unwrap();
 		Json(result)
+	}
+	
+	/// Delete a user
+	#[oai(path = "/users/:user_id", method = "delete")]
+	async fn delete_user(&self, database: Data<&Database>, user_id: Path<String>) -> PlainText<String> {
+		let collection = database.collection::<User>("users");
+		let object_id = ObjectId::from_str(user_id.as_str()).unwrap();
+		let result  = collection.delete_one(doc!{"_id": object_id}, None).await.unwrap();
+		let deleted = result.deleted_count;
+		
+		PlainText(format!("{deleted} user(s) deleted"))
+	}
+	
+	/// Get all Groups
+	#[oai(path = "/groups", method = "get")]
+	async fn get_groups(&self, database: Data<&Database>) -> Json<Vec<Group>> {
+		let collection = database.collection::<Group>("group");
+		let cursor = collection.find(None, None).await.unwrap();
+		let groups = cursor.try_collect::<Vec<Group>>().await.unwrap();
+		Json(groups)
 	}
 
 	/// Get group by ID
@@ -91,10 +110,33 @@ impl Api {
 		let result = collection.find_one(doc!{"_id": result.inserted_id}, None).await.unwrap().unwrap();
 		Json(result)
 	}
-
+	
+	/// Delete a group
+	#[oai(path = "/users/:group_id", method = "delete")]
+	async fn delete_group(&self, database: Data<&Database>, group_id: Path<String>) -> PlainText<String> {
+		let collection = database.collection::<Group>("groups");
+		let object_id = ObjectId::from_str(group_id.as_str()).unwrap();
+		let result  = collection.delete_one(doc!{"_id": object_id}, None).await.unwrap();
+		let deleted = result.deleted_count;
+		PlainText(format!("{deleted} group(s) deleted"))
+	}
+	
 	/// Add a user to a group
-	#[oai(path = "/groups/:group_id/members", method = "post")]
-	async fn add_group_member(&self, database: Data<&Database>, group_id: Path<String>, user_id: PlainText<String>) -> Json<Group> {
+	#[oai(path = "/groups/:group_id/members/:user_id", method = "put")]
+	async fn add_group_member(&self, database: Data<&Database>, group_id: Path<String>, user_id: Path<String>) -> Json<Group> {
+		let collection = database.collection::<Group>("groups");
+		let result = collection.update_one(
+			doc!{"_id" : group_id.0},
+			doc!{"$addToSet": {"members": user_id.0}},
+			None
+		).await.unwrap();
+		let result = collection.find_one(doc!{"_id": result.upserted_id}, None).await.unwrap().unwrap();
+		Json(result)
+	}
+	
+	/// Remove a user from a group
+	#[oai(path = "/groups/:group_id/members/:user_id", method = "delete")]
+	async fn remove_group_member(&self, database: Data<&Database>, group_id: Path<String>, user_id: Path<String>) -> Json<Group> {
 		let collection = database.collection::<Group>("groups");
 		let result = collection.update_one(
 			doc!{"_id" : group_id.0},
@@ -106,14 +148,14 @@ impl Api {
 	}
 
 	/// Add expense to a group
-	#[oai(path = "/groups/:group_id/expenses", method = "post")]
+	#[oai(path = "/groups/:group_id/expenses", method = "put")]
 	async fn add_expense(&self, database: Data<&Database>, group_id: Path<String>, expense: Json<Expense>) -> Json<Group> {
 		let collection = database.collection::<Group>("groups");
 		let serialized_expense = bson::to_bson(&expense.0).unwrap();
 		let document = serialized_expense.as_document().unwrap();
 		let result = collection.update_one(
 				doc!{"_id" : group_id.0},
-				doc!{"$push": {"expenses": document}},
+				doc!{"$addToSet": {"expenses": document}},
 				None
 			).await.unwrap();
 		let result = collection.find_one(doc!{"_id": result.upserted_id}, None).await.unwrap().unwrap();
