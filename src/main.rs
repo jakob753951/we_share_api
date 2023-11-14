2d039ecf49b63faf9cf97b9f6be8665585fab5eb
jakob753951 <jlm1999@live.dk>
2023-11-14 23:16:14 +0100

    Added env variables

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,20 @@ use poem::{EndpointExt, listener::TcpListener, Route, Server};
 use poem::middleware::AddData;
 use poem_openapi::OpenApiService;
 use endpoints::Api;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-	let mongodb = Client::with_uri_str("mongodb+srv://jakoblm:61lQ5Il8jf0n4BaO@cluster0.vh1fcbg.mongodb.net/?retryWrites=true&w=majority")
+	let mongodb = Client::with_uri_str(env::var("MONGODB_URI").unwrap())
 		.await
 		.unwrap()
 		.database("we_share");
 
+	let url = env::var("HOST_URL").unwrap();
+
 	let api_service =
 		OpenApiService::new(Api, "WeShare", "1.0.0")
-			.server("http://localhost:3000");
+			.server(env::var("DOCUMENTATION_TARGET_URL").unwrap());
 	let openapi = api_service.openapi_explorer();
 	let swagger = api_service.swagger_ui();
 	let app = Route::new()
@@ -25,8 +28,8 @@ async fn main() {
 		.nest("/swagger", swagger)
 		.with(AddData::new(mongodb));
 
-	println!("Running server on localhost:3000");
-	Server::new(TcpListener::bind("localhost:3000"))
+	println!("Running server on {url}");
+	Server::new(TcpListener::bind(url))
 		.run(app)
 		.await
 		.expect("Correct IO");
